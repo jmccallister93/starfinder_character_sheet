a826566514433cd4091364896b1e5160506bd6d3
jmccallister93 <jaredmccallister93@gmail.com>
2023-09-05 15:09:41 -0700

    Registration and login baseline setup

--- server/Supabase.js
+++ server/Supabase.js
@@ -1,33 +1,36 @@
-const express = require('express');
+const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
-console.log('Backend Supabase URL:', process.env.SUPABASE_URL);
-console.log('Backend Supabase Anon Key:', process.env.SUPABASE_ANON_KEY);
-
+console.log("Backend Supabase URL:", process.env.SUPABASE_URL);
+console.log("Backend Supabase Anon Key:", process.env.SUPABASE_ANON_KEY);
 
 const app = express();
 app.use(express.json());
-const cors = require('cors');
-app.use(cors({
-  origin: 'http://localhost:3000', // replace with your frontend application's URL
-  credentials: true,
-}));
-
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000", // replace with your frontend application's URL
+    credentials: true,
+  })
+);
 
 // Ensure Authentication Middleware
 function ensureAuthenticated(req, res, next) {
   if (req.session.user) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 // Register Route
-app.post('/register', async (req, res) => {
-  const { email, password } = req.body;  // Removed username
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body; // Removed username
 
   const { user, error } = await supabase.auth.signUp({
     email: email,
@@ -40,16 +43,19 @@ app.post('/register', async (req, res) => {
   }
 
   req.session.user = user;
-  res.redirect('/dashboard');
+  res.redirect("/dashboard");
 });
 
-
 // Login Route
-app.post('/login', async (req, res) => {
-  
-  console.log('Request Body:', req.body);
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
+  // Validate input
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password required" });
+  }
+
+  // Sign in user
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
@@ -57,30 +63,31 @@ app.post('/login', async (req, res) => {
 
   if (error) {
     console.log("Login error:", error);
-    return res.status(400).send(error.message);
+    res.status(401).json({ error: error.message });
   }
 
-  console.log("Login successful:", data);  // Log success and data.
-  res.status(200).json({ message: 'Login successful' });
+  console.log("Login successful");
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+  });
 });
 
-
 // Logout Route
-app.post('/logout', async (req, res) => {
+app.post("/logout", async (req, res) => {
   await supabase.auth.signOut();
   req.session.user = null;
-  res.redirect('/login');
+  res.redirect("/login");
 });
 
 // Dashboard Route
-app.get('/dashboard', ensureAuthenticated, (req, res) => {
+app.get("/dashboard", ensureAuthenticated, (req, res) => {
   res.send("Welcome to the dashboard!");
 });
 
 // ... (other routes and middleware)
 
 app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001/');
+  console.log("Server running on http://localhost:3001/");
 });
-
-
