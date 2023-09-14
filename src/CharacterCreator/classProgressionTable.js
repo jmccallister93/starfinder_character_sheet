f2d0aa141f215f8c85720327185123e847494e32
jmccallister93 <jaredmccallister93@gmail.com>
2023-09-14 16:51:54 -0700

    Still working through progressions

--- src/CharacterCreator/classProgressionTable.js
+++ src/CharacterCreator/classProgressionTable.js
@@ -5,27 +5,35 @@ function ClassProgressionTable({ className }) {
   const classData = classProgressionData[className];
 
   // Check which unique properties exist in the data
-  const hasSpells = classData.some(item => item.spells);
-  const hasAdaptiveStrike = classData.some(item => item.adaptive_strike);
-  const hasMajorForms = classData.some(item => item.majorForms);
-  const hasMinorForms = classData.some(item => item.minorForms);
-  const hasEsd = classData.some(item => item.esd);
+  const hasSpells = classData.some((item) => item.spells);
+  const hasAdaptiveStrike = classData.some((item) => item.adaptive_strike);
+  const hasMajorForms = classData.some((item) => item.majorForms);
+  const hasMinorForms = classData.some((item) => item.minorForms);
+  const hasEsd = classData.some((item) => item.esd);
+  const hasSolarArmor = classData.some((item) => item.solarArmor);
+  const hasSolarFlare = classData.some((item) => item.solarFlare);
+  const hasSolarShield = classData.some((item) => item.solarShield);
+  const hasSolarWeapon = classData.some((item) => item.solarWeapon);
 
   return (
     <Table variant="simple" mt={4}>
       <Thead>
         <Tr>
           <Th>Level</Th>
-          <Th>BAB</Th>
-          <Th>Fort</Th>
-          <Th>Ref</Th>
-          <Th>Will</Th>
+          <Th>Base Attack Bonus</Th>
+          <Th>Fortitude Save</Th>
+          <Th>Reflex Save</Th>
+          <Th>Will Save</Th>
           <Th>Special</Th>
           {hasSpells && <Th>Spells</Th>}
           {hasAdaptiveStrike && <Th>Adaptive Strike</Th>}
           {hasMajorForms && <Th>Major Forms</Th>}
           {hasMinorForms && <Th>Minor Forms</Th>}
-          {hasEsd && <Th>ESD</Th>}
+          {hasEsd && <Th>Entropic Strike Damage</Th>}
+          {hasSolarArmor && <Th>Solar Armor</Th>}
+          {hasSolarFlare && <Th>Solar Flare</Th>}
+          {hasSolarShield && <Th>Solar Sheild</Th>}
+          {hasSolarWeapon && <Th>Solar Weapon</Th>}
         </Tr>
       </Thead>
       <Tbody>
@@ -42,6 +50,10 @@ function ClassProgressionTable({ className }) {
             {hasMajorForms && <Td>{levelData.majorForms || "-"}</Td>}
             {hasMinorForms && <Td>{levelData.minorForms || "-"}</Td>}
             {hasEsd && <Td>{levelData.esd || "-"}</Td>}
+            {hasSolarArmor && <Td>{levelData.solarArmor || "-"}</Td>}
+            {hasSolarFlare && <Td>{levelData.solarFlare || "-"}</Td>}
+            {hasSolarShield && <Td>{levelData.solarShield || "-"}</Td>}
+            {hasSolarWeapon && <Td>{levelData.solarWeapon || "-"}</Td>}
           </Tr>
         ))}
       </Tbody>
